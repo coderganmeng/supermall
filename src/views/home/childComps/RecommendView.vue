f2a2fb5a105b3b1a6670cacdc69307d92c6d5e85
coderganmeng <1814860168@qq.com>
2021-05-21 20:50:43 +0800

    5.21更新

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,42 @@
+<!--  -->
+<template>
+  <div class="recommend">
+    <div class="recommend-item" v-for="item in recommends">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'RecommendView',
+  props: {
+    recommends: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.recommend {
+  display: flex;
+  text-align: center;
+  padding: 10px 0px 20px;
+  border-bottom: solid 10px #eee;
+}
+.recommend-item {
+  flex: 1;
+}
+.recommend-item img {
+  width: 78px;
+  height: 78px;
+  margin-bottom: 8px;
+}
+</style>
